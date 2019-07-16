2ed97c36d4c9c153d59c13e1e879491fb2f00ca9
Vikram Aroskar <vikram.aroskar@gmail.com>
2019-07-16 10:08:56 +0530

    Create sw_cached_site.js

---
+++ sw_cached_site.js
@@ -0,0 +1,53 @@
+const cacheName = 'v2';
+
+
+self.addEventListener('install', (event) => {
+    console.log('service worker: service worker installed')
+
+})
+
+self.addEventListener('activate', (event) => {
+    console.log('service worker: service worker activated')
+
+    //remove unwanted caches
+    event.waitUntil(
+        caches.keys()
+            .then(cacheNames => {
+                return Promise.all(
+                   cacheNames.map(cache =>{
+                     if (cache != cacheName) {
+                         console.log('service worker: clearing old cache')
+                         return caches.delete()
+                     }  
+                   })
+                )
+            })
+    )
+
+})
+
+
+
+self.addEventListener('fetch', (event) => {
+    console.log('service worker: fetch event called')
+    event.respondWith(
+        fetch(event.request)
+        .then(response => {
+            //make a coopy clone of response
+            const resClone = response.clone();
+            caches.open(cacheName)
+                .then(cache => {
+                    //caches add response
+                    caches.put(event.request, resClone);
+
+                })
+            return response;
+
+        })
+        .catch(() => {
+            caches.match(event.request)
+                .then(response => response)
+        })
+    )
+
+})
