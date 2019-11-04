c1b5b307492fc17a4919942c45da028a4de4a13e
Vikram Aroskar <vikram.aroskar@gmail.com>
2019-11-04 18:21:17 +0530

    add app with https and homepage in package.json

---
+++ react_admin_app2/precache-manifest.aa0d1506d3d8e1242119d47f767ab3f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780fa27d24d504f30b273716952774a4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7d4e1761e6c855176e09",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e35386df1631c8cab6a2",
+    "url": "./static/js/2.7ebb1451.chunk.js"
+  },
+  {
+    "revision": "7d4e1761e6c855176e09",
+    "url": "./static/js/main.8861027c.chunk.js"
+  },
+  {
+    "revision": "ad855208affa5085877b",
+    "url": "./static/js/runtime-main.b764eaaa.js"
+  }
+]);
