e1432962780fe94489d117930ceba4e9d5c0a5c2
Vikram Aroskar <vikram.aroskar@gmail.com>
2019-11-04 17:51:38 +0530

    add react admin demo

---
+++ react_admin_demo/precache-manifest.0f13bb7d2c78e530f6c6074066d5f9d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "197685b171a97a8ea0425040a38dd0b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f5c35ab0d36c52af4fe",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e35386df1631c8cab6a2",
+    "url": "/static/js/2.7ebb1451.chunk.js"
+  },
+  {
+    "revision": "9f5c35ab0d36c52af4fe",
+    "url": "/static/js/main.416f30b2.chunk.js"
+  },
+  {
+    "revision": "d5dfe1c843bc790fae0e",
+    "url": "/static/js/runtime-main.73d63f19.js"
+  }
+]);
