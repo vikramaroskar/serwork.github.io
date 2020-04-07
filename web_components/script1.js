fb661f4e2f52a146849dfb898872957ee95d748b
vikram <vikram.aroskar@gmail.com>
2020-04-07 12:35:29 +0530

    1 more level of commit

--- web_components/script1.js
+++ web_components/script1.js
@@ -4,11 +4,23 @@ template.innerHTML = `
         h3 {
             color: red;
         }
+        img {
+            height: 200px;
+            width: 200px;
+        }
     </style>
     <div class="card-component">
-
-        <h3></h3>
+        <img />
+        <div>
+            <h3></h3>
+            <div class="info">
+                <p>Email</p>
+                <p>Phone</p>
+            </div>
+            <button id="toggle-info"> Hide details</button>
+        </div>
     </div>
+
 
 `;
 
@@ -20,6 +32,9 @@ class CardComponent extends HTMLElement {
         this.shadowRoot.appendChild(template.content.cloneNode(true));
 
         this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
+        
+        this.shadowRoot.querySelector('img').innerText = this.getAttribute('picture');
+
 
         //this.innerHTML = `<style>h3 {color: blue;}</style><h3>${this.getAttribute('name')}</h3>`;
 
