01a1307a6dd4349ab7e4ba0fae21e30838bb23ae
vikram <vikram.aroskar@gmail.com>
2020-04-09 20:06:15 +0530

    working code and readme

--- web_components/script1.js
+++ web_components/script1.js
@@ -1,45 +1,77 @@
 const template = document.createElement('template');
 template.innerHTML = `
-    <style>
-        h3 {
-            color: red;
-        }
-        img {
-            height: 200px;
-            width: 200px;
-        }
-    </style>
-    <div class="card-component">
-        <img />
-        <div>
-            <h3></h3>
-            <div class="info">
-                <p>Email</p>
-                <p>Phone</p>
-            </div>
-            <button id="toggle-info"> Hide details</button>
-        </div>
+  <style>
+  .user-card {
+		font-family: 'Arial', sans-serif;
+		background: #f4f4f4;
+		width: 500px;
+		display: grid;
+		grid-template-columns: 1fr 2fr;
+		grid-gap: 10px;
+		margin-bottom: 15px;
+		border-bottom: darkorchid 5px solid;
+	}
+
+	.user-card img {
+		width: 100%;
+	}
+
+	.user-card button {
+		cursor: pointer;
+		background: darkorchid;
+		color: #fff;
+		border: 0;
+		border-radius: 5px;
+		padding: 5px 10px;
+	}
+  </style>
+  <div class="user-card">
+    <img />
+    <div>
+      <h3></h3>
+      <div class="info">
+        <p><slot name="email" /></p>
+        <p><slot name="phone" /></p>
+      </div>
+      <button id="toggle-info">Hide Info</button>
     </div>
-
-
+  </div>
 `;
 
-class CardComponent extends HTMLElement {
-    constructor() {
-        super();
+class UserCard extends HTMLElement {
+  constructor() {
+    super();
 
-        this.attachShadow({mode: 'open'});
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.showInfo = true;
 
-        this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
-        
-        this.shadowRoot.querySelector('img').innerText = this.getAttribute('picture');
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
+    this.shadowRoot.querySelector('img').src = this.getAttribute('avatar');
+  }
 
+  toggleInfo() {
+    this.showInfo = !this.showInfo;
 
-        //this.innerHTML = `<style>h3 {color: blue;}</style><h3>${this.getAttribute('name')}</h3>`;
+    const info = this.shadowRoot.querySelector('.info');
+    const toggleBtn = this.shadowRoot.querySelector('#toggle-info');
 
-
+    if(this.showInfo) {
+      info.style.display = 'block';
+      toggleBtn.innerText = 'Hide Info';
+    } else {
+      info.style.display = 'none';
+      toggleBtn.innerText = 'Show Info';
     }
+  }
+
+  connectedCallback() {
+    this.shadowRoot.querySelector('#toggle-info').addEventListener('click', () => this.toggleInfo());
+  }
+
+  disconnectedCallback() {
+    this.shadowRoot.querySelector('#toggle-info').removeEventListener();
+  }
 }
 
-window.customElements.define('card-component', CardComponent);
+window.customElements.define('user-card', UserCard);
