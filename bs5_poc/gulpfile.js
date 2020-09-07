3f48a1af0f70b009a3606c48cb99f884d0a07791
vikram <vikram.aroskar@gmail.com>
2020-09-07 17:23:04 +0530

    first cut bootstrap5

---
+++ bs5_poc/gulpfile.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const { watch, series, src, dest } = require("gulp");
+var gulp = require("gulp");
+
+var sass = require("gulp-sass");
+
+sass.compiler = require("node-sass");
+
+gulp.task("sass", async function () {
+  return gulp
+    .src("./scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css"));
+});
+
+gulp.task("sass:watch", async function () {
+  gulp.watch("./scss/**/*.scss", ["sass"]);
+});
+
+// The `clean` function is not exported so it can be considered a private task.
+// It can still be used within the `series()` composition.
+function clean(cb) {
+  // body omitted
+  cb();
+}
+
+// The `build` function is exported so it is public and can be run with the `gulp` command.
+// It can also be used within the `series()` composition.
+function build(cb) {
+  // body omitted
+  cb();
+}
+
+function streamTask() {
+  return src("*.js").pipe(dest("output"));
+}
+
+exports.build = build;
+exports.default = series(clean, build);
